c6514900a9095028c7f8fc2e4c8036d039d79cfa
Radhe Krishna <htank8866@gmail.com>
2024-06-24 16:46:25 +0530

    update

--- PRACTICE/basicreact/src/index.js
+++ PRACTICE/basicreact/src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Ensure this file exists or remove this line if not used
-// import './index.css'; 
-
-// If you have the logo.svg file and want to use it, make sure it's in the correct path
-//  import logo from './logo.svg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -16,6 +11,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you are not using reportWebVitals, you can remove this line
-// import reportWebVitals from './reportWebVitals';
-// reportWebVitals();
